5edff0d0144b5af111f8eaa637cebc5bc4275653
leeseunghee <shmocha787@gmail.com>
2016-12-28 21:15:42 +0900

    ffsdf

--- math/calculus/taking_derivatives/proofDerivatives.py
+++ math/calculus/taking_derivatives/proofDerivatives.py
@@ -16,16 +16,21 @@ f = lambdify(x, 3*x**2)
 #l=limit((f(x + delta) - f(x))/delta, delta, 0)
 # print(f(3))
 
-
 #2. trigonometric proof
 l=limit((sin(x + delta) - sin(x))/delta, delta, 0)
-
-
 
 #3. natural log proof
 limit((log(x + delta) - log(x))/delta, delta, 0)
 
+#4. log base b proof
+b=symbols('b')
+#limit((log(x + delta, 10) - log(x,10))/delta, delta, 0)
+limit((log(x + delta, b) - log(x,b))/delta, delta, 0)
 
+
+#5. e^x proof
+limit((exp(x + delta) - exp(x))/delta, delta, 0)
+ 
 # l=Limit(f(x+delta)-f(x)/delta,delta,0)
 
 

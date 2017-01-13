aed8844c731667c1513e178d8f9100e551420e2e
leeseunghee <shmocha787@gmail.com>
2017-01-13 13:44:47 +0900

    fsfsdff

---
+++ math/probability/combination.py
@@ -0,0 +1,17 @@
+'''
+Created on 2017. 1. 12.
+
+@author: lsh
+'''
+
+#import scipy.misc.comb
+#import scipy as sc
+#import scipy.misc.comb as co
+
+from scipy.special import comb
+#import scipy.special as sc
+
+n=60
+k=50
+
+print(comb(n,k))
